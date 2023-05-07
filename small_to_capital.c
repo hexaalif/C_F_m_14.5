f9430fa93cd99f25d55e36b9671b1ca08a8538a2
hexaalif <hexaalif2020@gmail.com>
2023-05-07 12:46:41 +0600

    function try

--- small_to_capital.c
+++ small_to_capital.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+int small_to_cap(char c)
+{
+    char cap = c - 32;
+    printf("%c", cap);
+    return cap;
+}
+
 int main()
 {
     char c;
     scanf("%c", &c);
-    printf("%c", c);
+    small_to_cap(c);
     return 0;
 }
